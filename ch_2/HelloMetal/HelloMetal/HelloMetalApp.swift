625873eed8efed181b0d17e6ac07bb0ce9e9c5cd
Shota Teranishi <cattleya100519b@gmail.com>
2023-01-28 21:41:43 +0900

    Initial commit

---
+++ ch_2/HelloMetal/HelloMetal/HelloMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMetalApp.swift
+//  HelloMetal
+//
+//  Created by Shota Teranishi on 2023/01/28.
+//
+
+import SwiftUI
+
+@main
+struct HelloMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
